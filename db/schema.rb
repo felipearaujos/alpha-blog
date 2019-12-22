4adac9de797611a8b2e5e43b2ed7b077e6ab8d98
Felipe Araujo <felipe.araujo.s@live.com>
2019-12-22 15:00:35 -0300

    create articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_22_171803) do
+ActiveRecord::Schema.define(version: 2019_12_22_173006) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
